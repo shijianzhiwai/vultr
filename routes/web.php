3021bf5ac2223402e563e6df116c2b998b52b4d9
xuhongxin <xuhongxin@luojilab.com>
2018-02-18 11:02:08 +0800

    【增加日志系统】

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('logs','\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
